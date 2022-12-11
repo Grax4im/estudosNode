35f4996ef60650740d19623b702c36edb7d3c01f
Porao <ratomorto@riseup.net>
2022-12-11 12:47:19 -0300

    gets and posts on the api

--- dia13 - Projeto API com Express - Parte 1/src/server.js
+++ dia13 - Projeto API com Express - Parte 1/src/server.js
@@ -1,13 +1,24 @@
 const PORT = 3003
-const { application } = require('express');
+const { application } = require('express')
 const EXPRESS = require('express')
 const APP = EXPRESS();
+const bd = require('.\\bd')
 
 APP.get('/produtos', (req, res, next) => {
-    res.send({
-        nome: 'Notebook',
-        preco: 123.45
+    res.send(bd.getProdutos())
+})
+
+APP.get('produtos/:id', (req,res,next) => {
+    res.send(bd.getproduto(req.params.id))
+})
+
+APP.post('/produtos', (req, res, next) => {
+    const produto = bd.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+
     })
+    res.send(produto)
 })
 
 APP.listen(PORT, () => {
